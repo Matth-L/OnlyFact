e75f0273fb9439ed6b2080fa0f278775796a5575
Evan Girard <xenon42320@gmail.com>
2023-11-29 20:59:21 +0100

    avancement front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { TopbarComponent } from './topbar/topbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { PostCardComponent } from './post-card/post-card.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    TopbarComponent,
+    LoginPageComponent,
+    PostCardComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,6 +28,8 @@ import {MatInputModule} from '@angular/material/input';
     MatSidenavModule,
     MatListModule,
     MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
